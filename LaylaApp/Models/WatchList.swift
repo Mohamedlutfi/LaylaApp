63befec423b4a3268a42e8a070f151ceaf2c3dc1
Diab-11 <dizk24wt@student.ju.se>
2026-03-10 00:58:09 +0100

    Create WatchList model with name, creation date, and movie relationship using SwiftData

--- LaylaApp/Models/WatchList.swift
+++ LaylaApp/Models/WatchList.swift
@@ -1,8 +1,15 @@
-//
-//  WatchList.swift
-//  LaylaApp
-//
-//  Created by zkaria Diab on 2026-03-10.
-//
-
 import Foundation
+import SwiftData
+
+@Model
+class WatchList {
+    var name: String
+    var createdAt: Date
+    @Relationship(deleteRule: .cascade) var movies: [SavedMovie]
+    
+    init(name: String) {
+        self.name = name
+        self.createdAt = Date()
+        self.movies = []
+    }
+}
